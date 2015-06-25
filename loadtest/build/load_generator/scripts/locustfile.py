c788e2f09a9f8ae60a31f310be8de68a620091fc
Marat Garafutdinov <maratoid@gmail.com>
2015-06-24 20:40:48 -0700

    in porgress

--- loadtest/build/load_generator/scripts/locustfile.py
+++ loadtest/build/load_generator/scripts/locustfile.py
@@ -2,39 +2,22 @@ import logging
 import gevent
 import pprint
 import os
+import json
+import flask
+import jinja2
+import time
+from flask import Flask, Response, render_template, send_from_directory, send_file
 from gevent.queue import Queue
-from locust import HttpLocust, TaskSet, task, events
-from influxdb.influxdb08 import InfluxDBClient
+from locust import HttpLocust, TaskSet, task, events, web
 
-host = os.getenv('INFLUXDB_HOST', 'influxdb')
-port = int(os.getenv('INFLUXDB_PORT', '8086'))
-user = os.getenv('INFLUXDB_USER', 'root')
-pw = os.getenv('INFLUXDB_PASSWORD', 'root')
-name = os.getenv('INFLUXDB_NAME', 'k8s')
+influx_queue = Queue(100000)
 
-influx_client = InfluxDBClient (host, port, user, pw, name)
-influx_queue = Queue()
-
-def influx_worker():
-  """The worker pops each item off the queue and sends it to influxdb."""
-  while True:
-      data = influx_queue.get()
-      json_body = None
-      if 'requests_per_second' in data:
-        json_body = [{
-          "Points": [[data['request_key'], data['requests_per_second'], data['epoch_time']]],
-          "Name": "trogdor_rps",
-          "Columns": ["request_key", "requests_per_second", "epoch_time"]
-        }]
-      else:
-        json_body = [{
-          "Points": [[data['request_key'], data['response_time'], data['epoch_time']]],
-          "Name": "trogdor_rts",
-          "Columns": ["request_key", "response_time", "epoch_time"]
-        }]
-
-      influx_client.write_points (json_body)
-      logging.debug('Wrote %s', pprint.pformat(data, 2))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+my_loader = jinja2.ChoiceLoader([
+    jinja2.FileSystemLoader(os.path.join(PROJECT_ROOT, 'templates')),
+    web.app.jinja_loader,
+])
+web.app.jinja_loader = my_loader
 
 def get_requests_per_second(stat, client_id):
     request = stat['method'] + stat['name'].replace('/', '-')
@@ -43,30 +26,14 @@ def get_requests_per_second(stat, client_id):
     for epoch_time, count in stat['num_reqs_per_sec'].items():
       influx_queue.put( {'request_key':request_key, 'requests_per_second':count, 'epoch_time':epoch_time} )
 
-def get_response_time(stat, client_id):
-    request = stat['method'] + stat['name'].replace('/', '-')
-
-    request_key = "locust.{0}.response_time.{1}".format(request, client_id)
-    epoch_time = int(stat['start_time'])
-
-    # flatten a dictionary of {time: count} to [time, time, time, ...]
-    response_times = []
-    for t, count in stat['response_times'].iteritems():
-        for _ in xrange(count):
-            response_times.append(t)
-
-    for response_time in response_times:
-      influx_queue.put( {'request_key':request_key, 'response_time':response_time, 'epoch_time':epoch_time} )
-
 def slave_report_log (client_id, data, ** kw):
   for stat in data['stats']:
-    get_response_time(stat, client_id)
     get_requests_per_second(stat, client_id)
 
 class JsonSerialization(TaskSet):
   @task(1)
   def json(self):
-    with self.client.get("/json", catch_response=True) as response:
+    with self.client.get("/", catch_response=True) as response:
       logging.debug('Response headers:')
       logging.debug(pprint.pformat(response.headers, 2))
       logging.debug('Response content:')
@@ -75,5 +42,47 @@ class JsonSerialization(TaskSet):
 class WebsiteUser(HttpLocust):
   task_set = JsonSerialization
 
-gevent.spawn(influx_worker)
+# Server sent events
+class ServerSentEvent:
+    FIELDS = ('event', 'data', 'id')
+    def __init__(self, data, event=None, event_id=None):
+        self.data = data
+        self.event = event 
+        self.id = event_id 
+
+    def encode(self):
+        if not self.data:
+            return ""
+        ret = []
+        for field in self.FIELDS:
+            entry = getattr(self, field) 
+            if entry is not None:
+                ret.extend(["%s: %s" % (field, line) for line in entry.split("\n")])
+        return "\n".join(ret) + "\n\n"
+
+@web.app.route("/dashboard")
+def my_dashboard():
+    return render_template('dashboard.html')
+
+@web.app.route("/files/<path:path>")
+def send_js(path):
+    return send_file(PROJECT_ROOT + '/files/' + path)
+
+@web.app.route("/stream")
+def stream():
+    def gen():
+        while True:
+            if influx_queue.empty():
+              time.sleep(0.05)
+              continue
+            queue_data = influx_queue.get()
+            data = json.dumps({"slave_id": queue_data['request_key'], 'rps': queue_data['requests_per_second'], 'epoch': queue_data['epoch_time']})
+            print(data)
+            ev = ServerSentEvent(data)
+            print(ev.encode())
+            yield ev.encode()
+            time.sleep(0.05)
+
+    return Response(gen(), mimetype="text/event-stream")
+
 events.slave_report += slave_report_log
